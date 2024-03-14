f293f2970419a70531c43550c1e9cf998fc82186
SacredJoshua <mcbratjo@gmail.com>
2024-03-14 01:16:15 -0600

    Modified script to make more clear of what it's doing

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 // Get all elements with the "xyz" class
 const sectionTitles = document.querySelectorAll('.section-title');
 const sectionTexts = document.querySelectorAll('.section-text');
+const slidesFromLeft = document.querySelectorAll('.slide-in-from-left');
+const slidesFromRight = document.querySelectorAll('.slide-in-from-right');
 
 
 function isInViewport(element) {
@@ -15,19 +17,15 @@ function isInViewport(element) {
     );
 }
 
-
-
-
-
 // Iterate through each section title and check if it's in the viewport
-sectionTitles.forEach(sectionTitle => {
-    gsap.set(sectionTitle, { opacity: 0, x:-30 });
+slidesFromRight.forEach(item => {
+    gsap.set(item, { opacity: 0, x:-30 });
     // Add a scroll event listener
     window.addEventListener('scroll', function () {
         // Check if the section title is in the viewport
-        if (isInViewport(sectionTitle)) {
+        if (isInViewport(item)) {
             // Animate the section title using GSAP
-            gsap.to(sectionTitle, { opacity: 1, x: 0, duration: 1 });
+            gsap.to(item, { opacity: 1, x: 0, duration: 1 });
         } else {
             // If not in the viewport, you can reset or hide the element
         }
@@ -35,14 +33,14 @@ sectionTitles.forEach(sectionTitle => {
 });
 
 // Iterate through each section title and check if it's in the viewport
-sectionTexts.forEach(text => {
-    gsap.set(text, { opacity: 0, x:30 }); // Set x to 30 for right slide
+slidesFromLeft.forEach(item => {
+    gsap.set(item, { opacity: 0, x:30 }); // Set x to 30 for right slide
     // Add a scroll event listener
     window.addEventListener('scroll', function () {
         // Check if the section title is in the viewport
-        if (isInViewport(text)) {
+        if (isInViewport(item)) {
             // Animate the section title using GSAP
-            gsap.to(text, { opacity: 1, x: 0, duration: 1});
+            gsap.to(item, { opacity: 1, x: 0, duration: 1});
         } else {
             // If not in the viewport, you can reset or hide the element
             // gsap.to(text, { opacity: 0, x:30 });
